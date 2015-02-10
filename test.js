c40192235ed6c9d80544103dd5cc8ec0a53d66ed
morganherlocker <morgan.herlocker@gmail.com>
2015-02-10 18:54:03 -0500

    additional fixtures

--- test.js
+++ test.js
@@ -4,6 +4,9 @@ var fs = require('fs');
 
 var triangle = JSON.parse(fs.readFileSync(__dirname + '/fixtures/in/triangle.geojson'));
 var rectangle = JSON.parse(fs.readFileSync(__dirname + '/fixtures/in/rectangle.geojson'));
+var convex = JSON.parse(fs.readFileSync(__dirname + '/fixtures/in/convex.geojson'));
+var concave = JSON.parse(fs.readFileSync(__dirname + '/fixtures/in/concave.geojson'));
+var romania = JSON.parse(fs.readFileSync(__dirname + '/fixtures/in/romania.geojson'));
 
 test('skeleton', function (t) {
 	var triskel = skeleton(triangle);
