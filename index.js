459a6100efb2e5f892bb8ab5f81bdc75833d0fb5
morganherlocker <morgan.herlocker@gmail.com>
2015-02-10 19:10:44 -0500

    compute distance of array

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = function () {
-	
+var destination = require('turf-destination');
+var bearing = require('turf-bearing');
+var extent = require('turf-extent');
+var distance = require('turf-distance');
+var point = require('turf-point');
+
+module.exports = function (poly) {
+	var hull = poly;
+	if(hull.type === 'Feature') hull = poly.geometry;
+	if(hull.type !== 'Polygon') throw new Error('Invalid input. Skeletons require polygon features or geometries.');
+
+	var bbox = extent(hull);
+	var horizontalDistance = distance(point([bbox[0], bbox[1]]), point([bbox[0], bbox[2]]), 'miles');
+	var verticalDistance = distance(point([bbox[0], bbox[1]]), point([bbox[1], bbox[1]]), 'miles');
+	var raydist = Math.max(horizontalDistance, verticalDistance) * 2;
 }
